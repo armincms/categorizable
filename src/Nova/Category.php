8c589c894ac550ca620280826c7e5118a9ed7e4f
Ismail Zare <zarehesmaiel@gmail.com>
2021-01-13 15:21:07 +0330

    Filter contetn type by the relation name

--- src/Nova/Category.php
+++ src/Nova/Category.php
@@ -139,7 +139,7 @@ abstract class Category extends Resource
                     ->placeholder(__('Select a user role.')),   
 
                 BooleanGroup::make(__('Content Type'), 'config->resources') 
-                    ->options($resources = SharedResource::resourceInformation($request, static::resourcesScope())->pluck('label', 'key'))
+                    ->options($resources = SharedResource::resourceInformation($request, static::resourcesScope())->pluck('label', 'relation'))
                     ->withMeta(array_filter([
                         'value' => $request->isCreateOrAttachRequest() ? $resources->map(function() {
                             return true;
